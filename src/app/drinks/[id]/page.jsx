c3cd4865df081343582cf96e4e17498f5ea0789f
b1winner <43081501+b1winner@users.noreply.github.com>
2024-04-29 06:55:56 -0400

    Edit

--- src/app/drinks/[id]/page.jsx
+++ src/app/drinks/[id]/page.jsx
@@ -3,11 +3,50 @@ import {notFound} from "next/navigation";
 
 //  needs to return:
 //  [{id: 1}, {id: 2}, {id: 3}, ...
+async function getAllData() {
+    const urls = [
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=b',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=c',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=d',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=e',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=f',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=g',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=h',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=i',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=j',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=k',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=l',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=m',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=n',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=o',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=p',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=q',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=r',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=s',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=t',
+        //'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=u', // no drinks
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=v',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=w',
+        //'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=x', // no drinks
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=y',
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=z'
+    ];
 
+    const fetchPromises = urls.map(url => fetch(url));
+    const responses = await Promise.all(fetchPromises);
+    const dataArrays = await Promise.all(responses.map(response => response.json()));
+    // Combine all the JSON data into one array
+    const combinedData = dataArrays.reduce((accumulator, currentData) => {
+        return accumulator.concat(currentData.drinks);
+    }, []);
+
+    return combinedData;
+}
 export async function generateStaticParams() {
-    const fetchResult = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a');
-    const posts = await fetchResult.json();
-    return posts.drinks.map(post => {
+    //const fetchResult = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a');
+    const posts = await getAllData();
+    return posts.map(post => {
         return {
             params: {
                 id: post.idDrink.toString()
